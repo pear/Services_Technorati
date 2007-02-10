5d67e0e747ba2ecd3e4152a64090ef60fdfa6364
James Stewart <jystewart@php.net>
2007-02-10 13:38:58 +0000

    Switch to package2.xml
    git-svn-id: http://svn.php.net/repository/pear/packages/Services_Technorati/trunk@229479 c90b9560-bf6c-de11-be94-00142212c4b1

--- test/Services_Technorati_Test.php
+++ test/Services_Technorati_Test.php
@@ -1,24 +1,27 @@
 <?php
 
+require '../Services/Technorati2.php';
+require 'PHPUnit.php';
+
 class Services_Technorati_Test extends PHPUnit_Testcase
 {
-	// contains the object handle of our Technorati class.
-	var $tapi;
-	
-	// contains our API key
-	var $key = "492a5486316000b6c9797e05cd64d846";
-	
+    // contains the object handle of our Technorati class.
+    var $tapi;
+    
+    // contains our API key
+    var $key = "492a5486316000b6c9797e05cd64d846";
+    
     /* constructor for test suite */
     function Services_Technorati_Test($name) {
-    	$this->PHPUnit_TestCase($name);
+        $this->PHPUnit_TestCase($name);
     }
     
     function setUp() {
-    	$this->tapi =& Services_Technorati::factory($this->key, null, 1.0);
+        $this->tapi =& Services_Technorati::factory($this->key, null, 1.0);
     }
     
     function tearDown() {
-    	unset($this->tapi);
+        unset($this->tapi);
     }
     
     /* Test that the factory brings the same result as going directly to the
@@ -26,15 +29,15 @@ class Services_Technorati_Test extends PHPUnit_Testcase
      * version
      */
     function testFactory() {
-    	$testTapi = new Services_Technorati($this->key, null, 1.0);
-    	$this->assertTrue($this->tapi == $testTapi);
+        $testTapi = new Services_Technorati($this->key, null, 1.0);
+        $this->assertTrue($this->tapi == $testTapi);
     }
     
     /* Test that the keyinfo query returns the two numeric values we need */
     function testKeyInfo() {
-    	$array = $this->tapi->keyInfo();
-    	$this->assertTrue(is_numeric($array['document']['result']['apiqueries'])
-    	    && is_numeric($array['document']['result']['maxqueries']));
+        $array = $this->tapi->keyInfo();
+        $this->assertTrue(is_numeric($array['document']['result']['apiqueries'])
+            && is_numeric($array['document']['result']['maxqueries']));
     }
     
     /* Test that the result of a cosmos query is either a relevant PEAR error or an
@@ -42,10 +45,10 @@ class Services_Technorati_Test extends PHPUnit_Testcase
      * passed is a URL.
      */
     function testCosmos() {
-    	$result = $this->tapi->cosmos('http://www.example.com');
-    	$this->assertTrue((is_array($result) && 
-    	    $result['document']['result']['url'] == 'http://www.example.com') or
-    	    (PEAR::isError($result) && $result->message == 'Technorati Response Error'));
+        $result = $this->tapi->cosmos('http://www.example.com');
+        $this->assertTrue((is_array($result) && 
+            $result['document']['result']['url'] == 'http://www.example.com') or
+            (PEAR::isError($result) && $result->message == 'Technorati Response Error'));
     }
 
     /* Test that the result of a search query is either a relevant PEAR error or an
@@ -54,9 +57,9 @@ class Services_Technorati_Test extends PHPUnit_Testcase
      */    
     function testSearch() {
         $result = $this->tapi->search('example');
-    	$this->assertTrue((is_array($result) && 
-    	    $result['document']['result']['query'] == 'example') or
-    	    (PEAR::isError($result) && $result->message == 'Technorati Response Error'));
+        $this->assertTrue((is_array($result) && 
+            $result['document']['result']['query'] == 'example') or
+            (PEAR::isError($result) && $result->message == 'Technorati Response Error'));
     }
 
     /* Test that the result of a getInfo query is either a relevant PEAR error or an
@@ -65,9 +68,9 @@ class Services_Technorati_Test extends PHPUnit_Testcase
      */        
     function testGetInfo() {
         $result = $this->tapi->getInfo('jystewart');
-    	$this->assertTrue((is_array($result) && 
-    	    $result['document']['result']['username'] == 'jystewart') or
-    	    (PEAR::isError($result) && $result->message == 'Technorati Response Error'));
+        $this->assertTrue((is_array($result) && 
+            $result['document']['result']['username'] == 'jystewart') or
+            (PEAR::isError($result) && $result->message == 'Technorati Response Error'));
     }
 
     /* Test that the result of an outbound query is either a relevant PEAR error or an
@@ -76,9 +79,9 @@ class Services_Technorati_Test extends PHPUnit_Testcase
      */    
     function testOutbound() {
         $result = $this->tapi->outbound('http://www.example.com');
-    	$this->assertTrue((is_array($result) && 
-    	    $result['document']['result']['url'] == 'http://www.example.com') or
-    	    (PEAR::isError($result) && $result->message == 'Technorati Response Error'));
+        $this->assertTrue((is_array($result) && 
+            $result['document']['result']['url'] == 'http://www.example.com') or
+            (PEAR::isError($result) && $result->message == 'Technorati Response Error'));
 
     }
 
@@ -88,9 +91,9 @@ class Services_Technorati_Test extends PHPUnit_Testcase
      */    
     function testBlogInfo() {
         $result = $this->tapi->blogInfo('http://www.example.com');
-    	$this->assertTrue((is_array($result) && 
-    	    $result['document']['result']['url'] == 'http://www.example.com') or
-    	    (PEAR::isError($result) && $result->message == 'Technorati Response Error'));
+        $this->assertTrue((is_array($result) && 
+            $result['document']['result']['url'] == 'http://www.example.com') or
+            (PEAR::isError($result) && $result->message == 'Technorati Response Error'));
     }
 
     /* Test that the result of a tag query is either a relevant PEAR error or an
@@ -99,9 +102,9 @@ class Services_Technorati_Test extends PHPUnit_Testcase
      */    
     function testTag() {
         $result = $this->tapi->tag('example');
-    	$this->assertTrue((is_array($result) && 
-    	    $result['document']['result']['query'] == 'example') or
-    	    (PEAR::isError($result) && $result->message == 'Technorati Response Error'));
+        $this->assertTrue((is_array($result) && 
+            $result['document']['result']['query'] == 'example') or
+            (PEAR::isError($result) && $result->message == 'Technorati Response Error'));
     }
 
     /* Test that the result of a TopTags query is either a relevant PEAR error or an
@@ -109,14 +112,14 @@ class Services_Technorati_Test extends PHPUnit_Testcase
      */        
     function testTopTags() {
         $result = $this->tapi->topTags();
-    	$this->assertTrue((is_array($result) && 
-    	    is_numeric($result['document']['result']['limit'])) or
-    	    (PEAR::isError($result) && $result->message == 'Technorati Response Error'));
+        $this->assertTrue((is_array($result) && 
+            is_numeric($result['document']['result']['limit'])) or
+            (PEAR::isError($result) && $result->message == 'Technorati Response Error'));
     }
     
     /* Send a query that doesn't exist and test for error */
     function test_sendRequest_404() {
-    	$this->assertTrue(PEAR::isError($this->tapi->_sendRequest("example")));
+        $this->assertTrue(PEAR::isError($this->tapi->_sendRequest("example")));
     }
     
     /* The ideal here would be to validate the return value against
@@ -124,34 +127,34 @@ class Services_Technorati_Test extends PHPUnit_Testcase
      * returns an array 
      */
     function test_sendRequest_success() {
-    	$result = $this->tapi->_sendRequest('cosmos', array('url' => 'http://www.example.com'));
+        $result = $this->tapi->_sendRequest('cosmos', array('url' => 'http://www.example.com'));
         $this->assertTrue((is_array($result) && 
-    	    $result['document']['result']['url'] == 'http://www.example.com') or
-    	    (PEAR::isError($result) && $result->message == 'Technorati Response Error'));
+            $result['document']['result']['url'] == 'http://www.example.com') or
+            (PEAR::isError($result) && $result->message == 'Technorati Response Error'));
     }
     
     function test_checkOptions_extraoptions() {
-    	$current = array(
-    	    'value1' => 1,
-    	    'value2' => 1,
-    	    'value3' => 1,
-    	    'valuea' => 1,
-    	    'valueb' => 1);
-    	$allowed = array('value1','value3','valueb');
-    	$this->assertTrue(
-    	    array_keys($this->tapi->_checkOptions($current, $allowed)) == $allowed);
+        $current = array(
+            'value1' => 1,
+            'value2' => 1,
+            'value3' => 1,
+            'valuea' => 1,
+            'valueb' => 1);
+        $allowed = array('value1','value3','valueb');
+        $this->assertTrue(
+            array_keys($this->tapi->_checkOptions($current, $allowed)) == $allowed);
     }
 
     function test_checkOptions_success() {
-    	$current = array(
-    	    'value1' => 1,
-    	    'value2' => 1,
-    	    'value3' => 1,
-    	    'valuea' => 1,
-    	    'valueb' => 1);
-    	$allowed = array('value1','value2','value3','valuea','valueb');
-    	$this->assertTrue(
-    	    array_keys($this->tapi->_checkOptions($current, $allowed)) == $allowed);
+        $current = array(
+            'value1' => 1,
+            'value2' => 1,
+            'value3' => 1,
+            'valuea' => 1,
+            'valueb' => 1);
+        $allowed = array('value1','value2','value3','valuea','valueb');
+        $this->assertTrue(
+            array_keys($this->tapi->_checkOptions($current, $allowed)) == $allowed);
     }
        
 }
